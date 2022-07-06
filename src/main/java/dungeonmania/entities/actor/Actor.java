45cb438a2a4984129d7bb54de7f73db58c054fc0
Kingston Chan <kingston.k.chan@student.unsw.edu.au>
2022-07-06 23:30:08 +1000

    Dungeonobject

--- src/main/java/dungeonmania/entities/actor/Actor.java
+++ src/main/java/dungeonmania/entities/actor/Actor.java
@@ -1,5 +1,39 @@
 package dungeonmania.entities.actor;
 
-public abstract class Actor {
+import dungeonmania.behaviours.visitor.VisitorBehaviour;
+import dungeonmania.entities.DungeonObject;
 
+public abstract class Actor extends DungeonObject {
+    private VisitorBehaviour visitorBehaviour;
+    private double healthPoints;
+    private int attackPoints;
+    private int defencePoints;
+
+    public void setVisitorBehaviour(VisitorBehaviour visitorBehaviour) {
+        this.visitorBehaviour = visitorBehaviour;
+    }
+
+    public double getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(double healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public void setAttackPoints(int attackPoints) {
+        this.attackPoints = attackPoints;
+    }
+
+    public int getDefencePoints() {
+        return defencePoints;
+    }
+
+    public void setDefencePoints(int defencePoints) {
+        this.defencePoints = defencePoints;
+    }
 }
