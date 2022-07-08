5d6eb313b01e574e36e12a34c5518c7c7156f2d3
kingston <>
2022-07-08 23:31:09 +1000

    Implemented checking whether goal has been achieved for all goals

--- src/main/java/dungeonmania/entities/goal/BoulderGoal.java
+++ src/main/java/dungeonmania/entities/goal/BoulderGoal.java
@@ -1,10 +1,19 @@
 package dungeonmania.entities.goal;
 
 import dungeonmania.entities.Dungeon;
+import dungeonmania.entities.staticobject.StaticObject;
 
 public class BoulderGoal implements Goal {
     @Override
     public boolean hasAchieved(Dungeon dungeon, StringBuilder allGoals) {
+        // for (StaticObject so : dungeon.getStaticObjects()) {
+        // if (so.getType().equals("switch") && !so.isActivated()) {
+        // allGoals.append(":boulders");
+        // return false;
+        // }
+        // }
+        // return true;
+
         allGoals.append(":boulders");
         return false;
     }
--- src/main/java/dungeonmania/entities/goal/EnemyGoal.java
+++ src/main/java/dungeonmania/entities/goal/EnemyGoal.java
@@ -5,6 +5,9 @@ import dungeonmania.entities.Dungeon;
 public class EnemyGoal implements Goal {
     @Override
     public boolean hasAchieved(Dungeon dungeon, StringBuilder allGoals) {
+        if (dungeon.getPlayer().getEnemiesDefeated() >= dungeon.getConfig("enemy_goal")) {
+            return true;
+        }
         allGoals.append(":enemies");
         return false;
     }
