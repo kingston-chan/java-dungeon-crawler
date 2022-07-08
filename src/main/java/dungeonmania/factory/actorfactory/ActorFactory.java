cca2a26e75b351219359076bb37079d456d18959
kingston <>
2022-07-08 12:38:10 +1000

    Stubbed dungeon/goal/battle classes and test designs for dungeon

--- src/main/java/dungeonmania/factory/actorfactory/ActorFactory.java
+++ src/main/java/dungeonmania/factory/actorfactory/ActorFactory.java
@@ -1,5 +1,13 @@
 package dungeonmania.factory.actorfactory;
 
-public class ActorFactory {
+import dungeonmania.entities.Dungeon;
+import dungeonmania.factory.DungeonObjectFactory;
+import dungeonmania.util.Position;
 
+public class ActorFactory implements DungeonObjectFactory {
+
+    @Override
+    public void create(Position position, String type, Dungeon dungeon) {
+
+    }
 }
