7c1392266aea7c2aeb91d97d43278d2e3dffe557
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-09 18:31:50 +1000

    add some buidlers of item

---
+++ src/main/java/dungeonmania/factory/itemfactory/KeyBuilder.java
@@ -0,0 +1,24 @@
+package dungeonmania.factory.itemfactory;
+
+import java.util.UUID;
+
+import dungeonmania.entities.Dungeon;
+import dungeonmania.entities.DungeonObject;
+import dungeonmania.entities.item.collectables.Key;
+import dungeonmania.util.Position;
+
+public class KeyBuilder implements ItemBuilder{
+
+    @Override
+    public DungeonObject buildItem(Position position, String type, Dungeon dungeon) {
+        Key key = new Key();
+        key.setDurability(-1);
+        key.setHostBehaviour(null);
+        key.setPosition(position);
+        key.setType(type);
+        key.setUniqueId(UUID.randomUUID().toString());
+
+        // TODO Auto-generated method stub
+        return key;
+    }
+}
