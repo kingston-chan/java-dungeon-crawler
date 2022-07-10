15350442e2d6737ae21011f3dbd996d6e88677a7
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 16:07:52 +1000

    changed the move method to also pass in Dungeon and NonPlayableActor

--- src/main/java/dungeonmania/behaviours/automatedmovement/MoveAwayFromPlayer.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/MoveAwayFromPlayer.java
@@ -1,12 +1,16 @@
 package dungeonmania.behaviours.automatedmovement;
 
+import dungeonmania.entities.Dungeon;
+import dungeonmania.entities.actor.nonplayableactor.NonPlayableActor;
 import dungeonmania.util.Position;
 
 public class MoveAwayFromPlayer implements AutomatedMovementBehaviour{
 
     @Override
-    public Position move(Position position) {
+    public Position move(Position position, Dungeon dungeon, NonPlayableActor actor) {
         return null;
     }
+
+
     
 }
--- src/main/java/dungeonmania/behaviours/automatedmovement/MoveTowardsPlayer.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/MoveTowardsPlayer.java
@@ -1,12 +1,15 @@
 package dungeonmania.behaviours.automatedmovement;
 
+import dungeonmania.entities.Dungeon;
+import dungeonmania.entities.actor.nonplayableactor.NonPlayableActor;
 import dungeonmania.util.Position;
 
 public class MoveTowardsPlayer implements AutomatedMovementBehaviour{
 
     @Override
-    public Position move(Position position) {
+    public Position move(Position position, Dungeon dungeon, NonPlayableActor actor) {
         return null;
     }
-    
+
+
 }
