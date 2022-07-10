db64b65aa7c6f112635fb11875e3bda00fa0a9b9
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 13:55:51 +1000

    added some more tests

--- src/test/java/dungeonmania/enemy/EnemyTest.java
+++ src/test/java/dungeonmania/enemy/EnemyTest.java
@@ -1,46 +1,86 @@
 package dungeonmania.enemy;
 
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class EnemyTest {
-    
-    @Test
-    public void testMercenaryMovesCloserToPlayer() {
 
+    @Nested
+    public class testSpiderMovement {
+
+        @Test
+        public void testSpiderMovesUpFirst() {
+
+        }
+
+        @Test
+        public void testSpiderMovesAroundSpawn() {
+
+        }
+
+        @Test
+        public void testSpiderReversesDirectionBoulder() {
+
+        }
     }
 
-    @Test
-    public void testSpiderMovement() {
+    @Nested
+    public class testSpiderUnaffected {
+        
+        @Test
+        public void testSpiderWalls() {
 
+        }
+
+        @Test
+        public void testSpiderDoors() {
+            
+        }
+
+        @Test
+        public void testSpiderPortals() {
+            
+        }
+
+        @Test
+        public void testSpiderSwitches() {
+            
+        }
+
+        @Test
+        public void testSpiderExits() {
+            
+        }
     }
 
     @Test
     public void testZombiesCanMoveDiagonally() {
 
     }
+
+    @Test
+    public void testMercenaryMovesCloserToPlayer() {
+
+    }
     
     @Test
-    public void testMercAndZombieMoveAwayInvis() {
+    public void testMercAndZombieMoveAwayInvince() {
 
     }
 
     @Test
-    public void testMercAndZombieMoveBackToDefault() {
-
+    public void testMercMoveRandomPlayerInvis() {
+        
     }
 
     @Test
-    public void testSpiderUnaffectedWalls() {
+    public void testMercAndZombieMovementBackToDefault() {
 
     }
 
     @Test
     public void testZombiesUnaffectedPortals() {
-
-    }
-
-    @Test
-    public void testMercenaryMoveCloserToPlayer() {
 
     }
 
