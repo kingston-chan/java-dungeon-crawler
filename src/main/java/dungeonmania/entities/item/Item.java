132191eb82288eda67f2afe1562420c9cd36894b
kingston <>
2022-07-10 22:30:43 +1000

    forgot to save

--- src/main/java/dungeonmania/entities/item/Item.java
+++ src/main/java/dungeonmania/entities/item/Item.java
@@ -6,17 +6,11 @@ import dungeonmania.entities.actor.player.Player;
 
 public abstract class Item extends DungeonObject {
 
-<<<<<<< HEAD
     public void collectedBy(Player player) {
         player.addToInventory(this);
     }
 
     public abstract boolean playerUse(Player player, Dungeon dungeon);
-=======
-    public abstract boolean playerUse(Player player);
-    public abstract boolean provideAttack(Player player);
-    public abstract boolean provideDefense(Player player);
->>>>>>> dde6ed2eff588145557401c74e82e0ecae700137
 
     @Override
     public boolean isInteractable() {
