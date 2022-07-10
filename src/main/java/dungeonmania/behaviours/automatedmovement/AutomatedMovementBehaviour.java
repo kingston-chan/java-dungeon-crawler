271cee38a8ccbbc0fa1341ed6fdab78ad6e650a0
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 11:23:10 +1000

    Added and stubbed all movement behaviours

--- src/main/java/dungeonmania/behaviours/automatedmovement/AutomatedMovementBehaviour.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/AutomatedMovementBehaviour.java
@@ -1,5 +1,9 @@
 package dungeonmania.behaviours.automatedmovement;
 
+import dungeonmania.util.Position;
+
 public interface AutomatedMovementBehaviour {
+
+    public Position move(Position position);
 
 }
