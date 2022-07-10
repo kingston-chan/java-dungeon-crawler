1fa3ba542a914bc2a19e837b9515521462f208ae
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 15:40:45 +1000

    Added new movement for ally FollowPlayer

---
+++ src/main/java/dungeonmania/behaviours/automatedmovement/FollowPlayer.java
@@ -0,0 +1,12 @@
+package dungeonmania.behaviours.automatedmovement;
+
+import dungeonmania.util.Position;
+
+public class FollowPlayer implements AutomatedMovementBehaviour{
+
+    @Override
+    public Position move(Position position) {
+        return null;
+    }
+    
+}
