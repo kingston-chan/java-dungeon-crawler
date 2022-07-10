4c29d12efb3fa3986a5021508f45d485556671c4
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 13:41:00 +1000

    added some empty enemy tests

---
+++ src/test/java/dungeonmania/enemy/EnemyTest.java
@@ -0,0 +1,48 @@
+package dungeonmania.enemy;
+
+import org.junit.jupiter.api.Test;
+
+public class EnemyTest {
+    
+    @Test
+    public void testMercenaryMovesCloserToPlayer() {
+
+    }
+
+    @Test
+    public void testSpiderMovement() {
+
+    }
+
+    @Test
+    public void testZombiesCanMoveDiagonally() {
+
+    }
+    
+    @Test
+    public void testMercAndZombieMoveAwayInvis() {
+
+    }
+
+    @Test
+    public void testMercAndZombieMoveBackToDefault() {
+
+    }
+
+    @Test
+    public void testSpiderUnaffectedWalls() {
+
+    }
+
+    @Test
+    public void testZombiesUnaffectedPortals() {
+
+    }
+
+    @Test
+    public void testMercenaryMoveCloserToPlayer() {
+
+    }
+
+
+}
