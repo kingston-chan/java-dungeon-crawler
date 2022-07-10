65b9f8d1157cb1bcf011c318d796c2c497defbf1
Djohan <d.roberto@student.unsw.edu.au>
2022-07-10 11:21:37 +1000

    changed enemy to abstract class, added subclasses for enemy

---
+++ src/main/java/dungeonmania/entities/actor/enemy/StandardEnemy.java
@@ -0,0 +1,12 @@
+package dungeonmania.entities.actor.enemy;
+
+import dungeonmania.behaviours.automatedmovement.AutomatedMovementBehaviour;
+
+public class StandardEnemy extends Enemy{
+    
+    @Override
+    public void update(AutomatedMovementBehaviour, HostBehaviour) {
+        
+    }
+
+}
