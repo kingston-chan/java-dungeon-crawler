b5862be76c3990c595e1839021664cfb8d137ea0
Djohan <d.roberto@student.unsw.edu.au>
2022-07-11 13:45:40 +1000

    changed builders for all enemy

--- src/main/java/dungeonmania/factory/actorfactory/ZombieToastBuilder.java
+++ src/main/java/dungeonmania/factory/actorfactory/ZombieToastBuilder.java
@@ -4,14 +4,13 @@ import java.util.UUID;
 
 import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.DungeonObject;
-import dungeonmania.entities.actor.nonplayableactor.Enemy;
 import dungeonmania.entities.actor.nonplayableactor.ZombieEnemy;
 import dungeonmania.util.Position;
 
 public class ZombieToastBuilder implements ActorBuilder {
     @Override
     public DungeonObject buildActor(Position position, String type, Dungeon dungeon) {
-        Enemy zombieToast = new ZombieEnemy();
+        ZombieEnemy zombieToast = new ZombieEnemy();
         zombieToast.setUniqueId(UUID.randomUUID().toString());
         zombieToast.setPosition(position);
         zombieToast.setType(type);
