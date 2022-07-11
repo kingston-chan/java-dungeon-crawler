50f56a69dbb3abffd96213b5b75bf623e803e0e6
kingston <>
2022-07-11 19:50:17 +1000

    Changed to using static dungeon mania controller get dungeon instead of passing dungeon around

--- src/main/java/dungeonmania/factory/actorfactory/ActorFactory.java
+++ src/main/java/dungeonmania/factory/actorfactory/ActorFactory.java
@@ -3,7 +3,6 @@ package dungeonmania.factory.actorfactory;
 import java.util.HashMap;
 import java.util.Map;
 
-import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.DungeonObject;
 import dungeonmania.factory.DungeonObjectFactory;
 import dungeonmania.util.Position;
@@ -19,9 +18,9 @@ public class ActorFactory implements DungeonObjectFactory {
     }
 
     @Override
-    public DungeonObject create(Position position, String type, Dungeon dungeon, String portalColour, int key) {
+    public DungeonObject create(Position position, String type, String portalColour, int key) {
         ActorBuilder actorBuilder = this.actorBuilders.get(type);
-        DungeonObject newDungeonObject = actorBuilder.buildActor(position, type, dungeon);
+        DungeonObject newDungeonObject = actorBuilder.buildActor(position, type);
         return newDungeonObject;
     }
 }
--- src/main/java/dungeonmania/factory/itemfactory/InvisibilityPotionBuilder.java
+++ src/main/java/dungeonmania/factory/itemfactory/InvisibilityPotionBuilder.java
@@ -2,7 +2,6 @@ package dungeonmania.factory.itemfactory;
 
 import java.util.UUID;
 
-import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.DungeonObject;
 import dungeonmania.entities.item.potions.InvisibilityPotion;
 import dungeonmania.util.Position;
@@ -10,13 +9,12 @@ import dungeonmania.util.Position;
 public class InvisibilityPotionBuilder implements ItemBuilder {
 
     @Override
-    public DungeonObject buildItem(Position position, String type, Dungeon dungeon) {
+    public DungeonObject buildItem(Position position, String type, int keyNum) {
         InvisibilityPotion invisibilityPotion = new InvisibilityPotion();
         invisibilityPotion.setPosition(position);
         invisibilityPotion.setType(type);
         invisibilityPotion.setUniqueId(UUID.randomUUID().toString());
 
-        // TODO Auto-generated method stub
         return invisibilityPotion;
     }
 }
