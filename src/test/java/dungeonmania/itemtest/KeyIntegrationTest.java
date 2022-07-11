4a61004cbc507384a5e67e4042a06654e003759f
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-11 23:08:10 +1000

    add bomb integration test and key test

---
+++ src/test/java/dungeonmania/itemtest/KeyIntegrationTest.java
@@ -0,0 +1,20 @@
+package dungeonmania.itemtest;
+
+import org.junit.jupiter.api.Test;
+
+public class KeyIntegrationTest {
+    @Test
+    public void testPickUpKey(){
+        
+    }
+
+    @Test
+    public void testOpenNotMatchedDoor(){
+
+    }
+
+    @Test
+    public void testOpenMatchedDoor(){
+
+    }
+}
