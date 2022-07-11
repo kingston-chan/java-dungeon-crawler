d3db5322cc752985731bafb573bdca60c45d9875
cmad73 <ksplashio@gmail.com>
2022-07-11 17:16:19 +1000

    Host/Visitor Interfaces added. Test Stubs for interactions added

--- src/main/java/dungeonmania/behaviours/host/HostBehaviour.java
+++ src/main/java/dungeonmania/behaviours/host/HostBehaviour.java
@@ -1,5 +1,19 @@
 package dungeonmania.behaviours.host;
 
-public interface HostBehaviour {
+import dungeonmania.entities.actor.nonplayableactor.NonPlayableActor;
+import dungeonmania.entities.actor.player.Player;
+import dungeonmania.entities.staticobject.boulder.Boulder;
 
+public interface HostBehaviour {
+  default boolean accept(Player player) {
+    return true;
+  }
+
+  default boolean accept(NonPlayableActor entity) {
+    return true;
+  }
+  
+  default boolean accept(Boulder boulder) {
+    return true;
+  }
 }
