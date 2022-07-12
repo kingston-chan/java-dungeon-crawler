45aaf49f1c4462a557c96aed6d32b4fe42cbaa60
Djohan <d.roberto@student.unsw.edu.au>
2022-07-13 09:34:08 +1000

    implemented random movement

--- src/main/java/dungeonmania/behaviours/automatedmovement/MoveRandomly.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/MoveRandomly.java
@@ -5,9 +5,14 @@ import dungeonmania.util.Position;
 
 public class MoveRandomly implements MovementBehaviour {
 
+    // actor should be able to move randomly (cardinal and diagonal) by one space
+
     @Override
-    public void move(NonPlayableActor npa) {
-        // TODO Auto-generated method stub
+    public void move(NonPlayableActor npa) { 
+        int x = (int) Math.random() * (1 - -1);
+        int y = (int) Math.random() * (1 - -1);
+
+        npa.setPosition(npa.getPosition().translateBy(x, y));
     }
 
 }
