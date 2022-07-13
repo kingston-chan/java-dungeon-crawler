15a21ebf696b683a78c5c199087e13e9e5bcce8e
Djohan <d.roberto@student.unsw.edu.au>
2022-07-13 15:59:32 +1000

    removed unused import in movementBehaviour

--- src/main/java/dungeonmania/behaviours/automatedmovement/MovementBehaviour.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/MovementBehaviour.java
@@ -1,8 +1,6 @@
 package dungeonmania.behaviours.automatedmovement;
 
-import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.actor.nonplayableactor.NonPlayableActor;
-import dungeonmania.util.Position;
 
 public interface MovementBehaviour {
 
