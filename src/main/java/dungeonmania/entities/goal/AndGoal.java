98a3db594ceae63156d36794c7a2736ad510fd4b
kingston <>
2022-07-13 17:15:34 +1000

    Added composite pattern to track goals

---
+++ src/main/java/dungeonmania/entities/goal/AndGoal.java
@@ -0,0 +1,18 @@
+package dungeonmania.entities.goal;
+
+public class AndGoal extends ComplexGoal {
+    private Goal subGoal1;
+    private Goal subGoal2;
+
+    @Override
+    public boolean hasAchieved() {
+        return this.subGoal1.hasAchieved() && this.subGoal2.hasAchieved();
+    }
+
+    @Override
+    public void addSubgoals(Goal subGoal1, Goal subGoal2) {
+        this.subGoal1 = subGoal1;
+        this.subGoal2 = subGoal2;
+    }
+
+}
