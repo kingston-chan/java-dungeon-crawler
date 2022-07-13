bfd410f83ea8179656a911ad906eeaf7e177919c
Djohan <d.roberto@student.unsw.edu.au>
2022-07-13 10:45:50 +1000

    implement moveTowardsPlayer (using BFS)

--- src/main/java/dungeonmania/behaviours/automatedmovement/MoveTowardsPlayer.java
+++ src/main/java/dungeonmania/behaviours/automatedmovement/MoveTowardsPlayer.java
@@ -1,13 +1,72 @@
 package dungeonmania.behaviours.automatedmovement;
 
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.entities.Dungeon;
+import dungeonmania.entities.DungeonObject;
 import dungeonmania.entities.actor.nonplayableactor.NonPlayableActor;
+import dungeonmania.entities.actor.player.Player;
 import dungeonmania.util.Position;
+
+import java.util.Queue;
 
 public class MoveTowardsPlayer implements MovementBehaviour {
 
     @Override
     public void move(NonPlayableActor npa) {
-        // TODO Auto-generated method stub
+        Dungeon dungeon = DungeonManiaController.getDungeon();
+        Player player = dungeon.getPlayer();
+        
+        // using BFS
+        Position playerPosition = player.getPosition();     
+        Queue<Position> queue = new LinkedList<>();
+        Map<Position, Position> visited= new HashMap<>();    //adjList
+        boolean playerFound = false; 
+
+        visited.put(npa.getPosition(), null);
+        queue.add(npa.getPosition());
+
+        while(!(queue.isEmpty()) && !(playerFound)) {
+            Position curr = queue.poll();
+            if (curr.equals(playerPosition)) {
+                playerFound = true;
+            } else {
+                // checks for all adjacent valid positions and adds to the queue and visited
+                for (Position pos : curr.getAdjacentPositions()) {
+                    // checking whether adj pos are valid or not
+                    //
+                    // List<DungeonObject> occupant = dungeon.getObjectsAtPosition(pos);
+                    // for (DungeonObject object : occupant) {
+                    //      if (!(visited.containsKey(pos)) && object.accept(npa)) {
+                    //          visited.put(pos, curr);
+                    //          queue.add(pos);
+                    //      }
+                    // }
+                    
+                    // temporary
+                    if (!(visited.containsKey(pos))) {
+                        visited.put(pos, curr);
+                        queue.add(pos);
+                    }
+                }
+            }
+        }
+        
+        // get the step to the closest path
+        Position next = visited.get(playerPosition);
+        while (visited.get(next) != null) {
+            next = visited.get(next);
+        }
+
+        // set Position  
+        Position diff = Position.calculatePositionBetween(next, npa.getPosition());
+        npa.setPosition(diff);
+
     }
 
 }
