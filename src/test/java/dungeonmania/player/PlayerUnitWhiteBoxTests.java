9283f9695cf9a08b9911f7ba668ed8e7013ea45a
kingston <>
2022-07-14 17:57:20 +1000

    Uncommented some old tests and wrote some new ones

--- src/test/java/dungeonmania/player/PlayerUnitWhiteBoxTests.java
+++ src/test/java/dungeonmania/player/PlayerUnitWhiteBoxTests.java
@@ -1,27 +1,14 @@
 package dungeonmania.player;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static dungeonmania.TestUtils.getPlayer;
-import static dungeonmania.TestUtils.getEntities;
-import static dungeonmania.TestUtils.getInventory;
-import static dungeonmania.TestUtils.getGoals;
-import static dungeonmania.TestUtils.countEntityOfType;
-import static dungeonmania.TestUtils.getValueFromConfigFile;
 import static dungeonmania.TestUtils.assertListAreEqualIgnoringOrder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Nested;
 
 import dungeonmania.DungeonManiaController;
 import dungeonmania.entities.Dungeon;
@@ -29,17 +16,6 @@ import dungeonmania.entities.actor.nonplayableactor.Mercenary;
 import dungeonmania.entities.actor.player.Player;
 import dungeonmania.entities.item.Item;
 import dungeonmania.entities.item.Key;
-import dungeonmania.entities.item.collectables.Arrows;
-import dungeonmania.entities.item.collectables.Treasure;
-import dungeonmania.entities.item.collectables.Wood;
-import dungeonmania.entities.item.equipment.Sword;
-import dungeonmania.exceptions.InvalidActionException;
-import dungeonmania.response.models.BattleResponse;
-import dungeonmania.response.models.DungeonResponse;
-import dungeonmania.response.models.EntityResponse;
-import dungeonmania.response.models.ItemResponse;
-import dungeonmania.response.models.RoundResponse;
-import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 /**
@@ -151,8 +127,8 @@ public class PlayerUnitWhiteBoxTests {
         player.setPosition(new Position(1, 1));
         assertTrue(player.interact(mercId));
         assertEquals(1, player.getNumAllies());
-        // assertTrue(merc.isAlly());
-        // assertFalse(merc.isInteractable());
+        assertTrue(merc.isAlly());
+        assertFalse(merc.isInteractable());
     }
 
     @Test
