b2ce3e9880c24e66c2ff33c5a2b187349ab2adf7
Gabriel Gotsis <z5362640@ad.unsw.edu.au>
2022-07-15 20:04:33 +1000

    framework done

---
+++ src/test/java/dungeonmania/staticobject/BoulderInteractionTests.java
@@ -0,0 +1,15 @@
+package dungeonmania.staticobject;
+
+import org.junit.jupiter.api.Test;
+
+public class BoulderInteractionTests {
+  @Test
+  public void blouderVisitsFloorSwitchTest() {
+
+  }
+
+  @Test
+  public void blouderVisitsOtherEntitiesTest() {
+
+  }
+}
