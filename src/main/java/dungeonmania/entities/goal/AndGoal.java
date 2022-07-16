d94fe9cc86353a744689eb1ced1600525f06517d
kingston <>
2022-07-16 14:22:18 +1000

    Moved subgoal fields into abstract parent class

--- src/main/java/dungeonmania/entities/goal/AndGoal.java
+++ src/main/java/dungeonmania/entities/goal/AndGoal.java
@@ -1,25 +1,22 @@
 package dungeonmania.entities.goal;
 
 public class AndGoal extends ComplexGoal {
-    private Goal subGoal1;
-    private Goal subGoal2;
-
     @Override
     public boolean hasAchieved() {
-        return this.subGoal1.hasAchieved() && this.subGoal2.hasAchieved();
+        return getSubGoal1().hasAchieved() && getSubGoal2().hasAchieved();
     }
 
     @Override
     public void addSubgoals(Goal subGoal1, Goal subGoal2) {
-        this.subGoal1 = subGoal1;
-        this.subGoal2 = subGoal2;
+        setSubGoal1(subGoal1);
+        setSubGoal2(subGoal2);
     }
 
     @Override
     public String toString() {
-        if (!this.subGoal1.toString().isEmpty() && !this.subGoal2.toString().isEmpty()) {
-            return "(" + this.subGoal1.toString() + " AND " + this.subGoal2.toString() + ")";
+        if (!getSubGoal1().toString().isEmpty() && !getSubGoal2().toString().isEmpty()) {
+            return "(" + getSubGoal1().toString() + " AND " + getSubGoal2().toString() + ")";
         }
-        return this.subGoal1.toString() + this.subGoal2.toString();
+        return getSubGoal1().toString() + getSubGoal2().toString();
     }
 }
