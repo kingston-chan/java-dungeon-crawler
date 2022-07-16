61ebf448ff3d142b3b5e46b3cd41a2b9e69ca0c2
Kingston Chan <kingston.k.chan@student.unsw.edu.au>
2022-07-16 14:49:42 +1000

    Item

--- src/main/java/dungeonmania/entities/item/Item.java
+++ src/main/java/dungeonmania/entities/item/Item.java
@@ -4,15 +4,10 @@ import dungeonmania.entities.DungeonObject;
 import dungeonmania.entities.actor.player.Player;
 
 public abstract class Item extends DungeonObject {
-    public abstract boolean playerUse(Player player);
-
-    @Override
-    public void doAccept(Player player) {
-        player.visit(this);
-    }
-
     @Override
     public boolean isInteractable() {
         return false;
     }
+
+    public abstract boolean playerUse(Player player);
 }
