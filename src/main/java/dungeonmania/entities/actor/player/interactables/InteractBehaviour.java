4cd50d799acc493766f80507d559f583bb17c027
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-07-20 07:39:47 +1000

    Milestone 2 Automarking

--- src/main/java/dungeonmania/entities/actor/player/interactables/InteractBehaviour.java
+++
@@ -1,7 +0,0 @@
-package dungeonmania.entities.actor.player.interactables;
-
-import dungeonmania.entities.actor.player.Player;
-
-public interface InteractBehaviour {
-    public boolean interact(Player player, String interactingWithId);
-}
