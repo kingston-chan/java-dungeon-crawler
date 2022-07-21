d70d88020c45b6e2bef9bc0df4253ee9f1c26b63
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-21 12:04:39 +1000

    stub buildable unit tests

---
+++ src/test/java/dungeonmania/morebuildables/SceptreUnitTest.java
@@ -0,0 +1,46 @@
+package dungeonmania.morebuildables;
+
+import org.junit.jupiter.api.Test;
+
+public class SceptreUnitTest {
+    @Test
+    public void testBuildSceptreWithWood_Key_SunStone(){
+
+    }
+    
+    @Test
+    public void testBuildSceptreWithWood_Treasure_SunStone(){
+
+    }
+
+    @Test
+    public void testBuildSceptreWithArrows_Key_SunStone(){
+
+    }
+
+    @Test
+    public void testBuildSceptreWithArrows_Treasure_SunStone(){
+
+    }
+
+    @Test
+    public void testFailedToBuildSceptre(){
+
+    }
+
+    @Test
+    public void testMindControlUsingSceptreSuccessInDuration(){
+
+    }
+
+    @Test
+    public void testSceptreLimitedDurability(){
+
+    }
+
+    @Test
+    public void testMindControlInvalidObject(){
+
+    }
+
+}
