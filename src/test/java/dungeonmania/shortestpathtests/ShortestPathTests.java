b44afad7d0f48bf1f6d0bccaeb71baf2a4b3e148
kingston <>
2022-07-21 15:26:39 +1000

    Stubbed junit tests for shortest path test/swamp tiles

---
+++ src/test/java/dungeonmania/shortestpathtests/ShortestPathTests.java
@@ -0,0 +1,40 @@
+package dungeonmania.shortestpathtests;
+
+import org.junit.jupiter.api.Test;
+
+public class ShortestPathTests {
+    @Test
+    public void testFindShortestPath() {
+
+    }
+
+    @Test
+    public void testFindShortestPathSwampTile() {
+
+    }
+
+    @Test
+    public void testFindShortestPathPortal() {
+
+    }
+
+    @Test
+    public void testFindShortestPathPortalSwampTile() {
+
+    }
+
+    @Test
+    public void testShortestPathIncludesSwampTile() {
+
+    }
+
+    @Test
+    public void testChangeDirectionWhenDoorOpened() {
+
+    }
+
+    @Test
+    public void testChangeDirectionWhenBoulderPushed() {
+
+    }
+}
---
+++ src/test/java/dungeonmania/staticobject/SwampTileTests.java
@@ -0,0 +1,30 @@
+package dungeonmania.staticobject;
+
+import org.junit.jupiter.api.Test;
+
+public class SwampTileTests {
+    @Test
+    public void testSwampTileMercenary() {
+
+    }
+
+    @Test
+    public void testSwampTileZombie() {
+
+    }
+
+    @Test
+    public void testSwampTileSpider() {
+
+    }
+
+    @Test
+    public void testSwampTileHydra() {
+
+    }
+
+    @Test
+    public void testSwampTileAssassin() {
+
+    }
+}
