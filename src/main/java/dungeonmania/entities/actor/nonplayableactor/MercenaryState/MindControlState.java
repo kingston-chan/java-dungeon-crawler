37085c2b4a64be68dfc1a79d5b18cf33619b9651
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-21 12:06:20 +1000

    stub mind control state

---
+++ src/main/java/dungeonmania/entities/actor/nonplayableactor/MercenaryState/MindControlState.java
@@ -0,0 +1,38 @@
+package dungeonmania.entities.actor.nonplayableactor.MercenaryState;
+
+import dungeonmania.behaviours.movement.MovementBehaviour;
+
+public class MindControlState implements MercenaryState {
+    private int counter;
+
+    @Override
+    public boolean canInteract() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void updateMovement(MovementBehaviour movementBehaviour) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isAlly() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void recruit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mindcontrol() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
