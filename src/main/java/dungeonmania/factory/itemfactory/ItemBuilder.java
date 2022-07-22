c4fd38d34f6db56cc32fcd4edb8da14bd1086cec
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-07-23 09:22:50 +1000

    Automarking Results v2

--- src/main/java/dungeonmania/factory/itemfactory/ItemBuilder.java
+++
@@ -1,7 +0,0 @@
-package dungeonmania.factory.itemfactory;
-
-import dungeonmania.util.Position;
-
-public interface ItemBuilder {
-    public void buildItem(Position position, String type, int keyNum);
-}
