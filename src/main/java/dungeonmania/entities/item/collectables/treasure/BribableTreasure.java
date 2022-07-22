1b0faa44992f00cc56dccfca6d68dbf72796091d
Gabriel Gotsis <g.gotsis@student.unsw.edu.au>
2022-07-23 00:29:17 +1000

    Sun stone

--- src/main/java/dungeonmania/entities/item/collectables/treasure/BribableTreasure.java
+++ src/main/java/dungeonmania/entities/item/collectables/treasure/BribableTreasure.java
@@ -2,4 +2,8 @@ package dungeonmania.entities.item.collectables.treasure;
 
 public class BribableTreasure extends Treasure {
     
+    @Override
+    public boolean isBribableCurrency() {
+        return true;
+    }
 }
