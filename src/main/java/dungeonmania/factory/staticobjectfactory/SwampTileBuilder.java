bface540ecef81f2a5aada6040357104f8626fdc
kingston <>
2022-07-22 16:32:58 +1000

    Added stubbed builder for swamp tile

---
+++ src/main/java/dungeonmania/factory/staticobjectfactory/SwampTileBuilder.java
@@ -0,0 +1,12 @@
+package dungeonmania.factory.staticobjectfactory;
+
+import dungeonmania.util.Position;
+
+public class SwampTileBuilder implements StaticObjectBuilder {
+
+    @Override
+    public void buildStaticObject(Position position, String type, String portalColour, int key) {
+
+    }
+
+}
