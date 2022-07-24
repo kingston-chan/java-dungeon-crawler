6f1ca57157c4d727fc9a62968fe4149194884a57
kingston <>
2022-07-24 17:28:30 +1000

    Implemented hydra class

--- src/main/java/dungeonmania/entities/actor/nonplayableactor/Hydra.java
+++ src/main/java/dungeonmania/entities/actor/nonplayableactor/Hydra.java
@@ -1,7 +1,21 @@
 package dungeonmania.entities.actor.nonplayableactor;
 
-public class Hydra extends ZombieToast{
+import dungeonmania.entities.actor.player.Player;
+import dungeonmania.entities.battle.Battle;
 
-    
-    
+public class Hydra extends SpecialCreature {
+    @Override
+    public void doAccept(Player player) {
+        player.visit(this);
+    }
+
+    @Override
+    public void visit(Player player) {
+        Battle battle = new Battle(this.getType(), this.getHealthPoints(), player.getHealthPoints());
+        battle.simulateHydraBattle(player, this);
+    }
+
+    @Override
+    public void visitInvisiblePlayer(Player player) {
+    }
 }
