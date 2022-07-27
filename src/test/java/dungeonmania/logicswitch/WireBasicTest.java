d6f6a0edee51cc7c5ea77bde42a52e1073b249f2
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-27 16:43:11 +1000

    initialize first test

--- src/test/java/dungeonmania/logicswitch/WireBasicTest.java
+++ src/test/java/dungeonmania/logicswitch/WireBasicTest.java
@@ -1,12 +1,15 @@
 package dungeonmania.logicswitch;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
+
+import static dungeonmania.TestUtils.countEntityOfType;
 
 public class WireBasicTest {
     @Test
@@ -24,6 +27,7 @@ public class WireBasicTest {
         dmc.tick(Direction.RIGHT);
         DungeonResponse resp = dmc.tick(Direction.RIGHT);
         // player position: 7,1
+        // bomb position: 7,1
         String bomb_id = resp.getEntities()
                                             .stream()
                                             .filter(e->e.getType().equals("bomb"))
@@ -37,7 +41,12 @@ public class WireBasicTest {
         dmc.tick(Direction.LEFT);
         dmc.tick(Direction.LEFT);
         dmc.tick(Direction.DOWN);
-        dmc.tick(Direction.RIGHT);
+        resp = dmc.tick(Direction.RIGHT);
+        // activitate bomb and explode
+        int count = countEntityOfType(resp, "bomb");
+        assertEquals(count, 0);
 
+        count = countEntityOfType(resp, "wire");
+        assertEquals(count, 1);
     }
 }
