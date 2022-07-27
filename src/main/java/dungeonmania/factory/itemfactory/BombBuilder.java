cc7b0179c992bec9909a3529eb902d2e9490b213
Yuhao Guo <YuhaoGuo@unsw.edu.au>
2022-07-27 18:23:00 +1000

    add logicBomb generation in BombBuilder

--- src/main/java/dungeonmania/factory/itemfactory/BombBuilder.java
+++ src/main/java/dungeonmania/factory/itemfactory/BombBuilder.java
@@ -7,16 +7,24 @@ import org.json.JSONObject;
 import dungeonmania.DungeonManiaController;
 import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.item.Bomb;
+import dungeonmania.entities.item.LogicBomb;
 import dungeonmania.factory.FactoryHelpers;
+import dungeonmania.factory.LogicFactory;
 
 public class BombBuilder implements ItemBuilder {
     @Override
     public void buildItem(JSONObject item) {
         Dungeon dungeon = DungeonManiaController.getDungeon();
-        Bomb bomb = new Bomb();
-        bomb.setPosition(FactoryHelpers.extractPosition(item));
-        bomb.setType(FactoryHelpers.extractType(item));
-        bomb.setUniqueId(UUID.randomUUID().toString());
-        dungeon.addDungeonObject(bomb.getUniqueId(), bomb);
+        Bomb bomb;
+        try {
+            String logic = FactoryHelpers.extractLogic(item);
+            bomb = new LogicBomb(LogicFactory.getLogicType(logic));
+        } catch (Exception e) {
+            bomb = new Bomb();
+        }
+            bomb.setPosition(FactoryHelpers.extractPosition(item));
+            bomb.setType(FactoryHelpers.extractType(item));
+            bomb.setUniqueId(UUID.randomUUID().toString());
+            dungeon.addDungeonObject(bomb.getUniqueId(), bomb);
     }
 }
