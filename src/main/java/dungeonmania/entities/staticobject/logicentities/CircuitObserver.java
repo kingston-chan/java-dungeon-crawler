9ed933a72ea0267c32cad0f4ebf7103d5e18e68e
kingston <>
2022-07-31 16:28:14 +1000

    Removed unused method

--- src/main/java/dungeonmania/entities/staticobject/logicentities/CircuitObserver.java
+++ src/main/java/dungeonmania/entities/staticobject/logicentities/CircuitObserver.java
@@ -1,9 +1,5 @@
 package dungeonmania.entities.staticobject.logicentities;
 
-import dungeonmania.util.Position;
-
 public interface CircuitObserver {
     public void updateLogic();
-
-    public Position getPosition();
 }
